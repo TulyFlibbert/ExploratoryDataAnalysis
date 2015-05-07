779b0c0a295e448a1e3bf4b8b4df847f6dc274f2
TulyFlibbert <TulyFlibbert@users.noreply.github.com>
2015-05-07 12:27:17 -0500

    Create plot2.R
    R file to create Plot 2 for Project 1

---
+++ plot2.R
@@ -0,0 +1,25 @@
+
+#Read the txt file only to approx. 2/3/2007
+
+data <- read.delim("household_power_consumption.txt", sep=";", nrows=70000)
+
+#Transform date and time to standard forms
+data <- transform(data, Date = as.Date(Date, "%d/%m/%Y"))
+data <- transform(data, Time = strptime(Time, "%H:%M:%S"))
+
+data <- transform(data, Global_active_power = as.double(Global_active_power))
+data <- transform(data, Sub_metering_1 = as.double(Sub_metering_1))
+data <- transform(data, Sub_metering_2 = as.double(Sub_metering_2))
+data <- transform(data, Sub_metering_3 = as.double(Sub_metering_3))
+data <- transform(data, Global_reactive_power = as.double(Global_reactive_power))
+
+#Subset the appropriate timeframe
+day1 <- data[data$Date == as.Date("2007-02-01"), ]
+day2 <- data[data$Date == as.Date("2007-02-02"), ]
+
+period <- rbind(day1, day2)
+
+#plot 2
+png(filename = "plot2.png")
+plot(period$Global_active_power, period$time, main = "Global Active Power", ylab = "Global Active Power (kW)", type = "l")
+dev.off()
